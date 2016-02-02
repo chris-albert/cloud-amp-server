7dd983a5d4e2b4e7c2f998c000020634a075d79f
creasetoph <creasetoph@creasetoph.com>
2016-02-01 19:46:48 -0800

    Initial add

---
+++ app.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var PlayMusic = require('./playmusic');
+var _ = require('lodash');
+
+//http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2013/11/Winampmain.png
+
+var cache = null;
+
+var GooglePlayService = {
+  /**
+   * Takes an artist and groups by albums and tracks
+   * @param artistTracks Array of tracks for an artist
+   */
+  formatArtist(artistTracks) {
+    if(_.isEmpty(artistTracks)) {
+      return {name: 'Empty'};
+    }
+    var albumTracks = _.toArray(_.groupBy(artistTracks,'album'));
+    var albums = _.map(albumTracks,tracks => {
+      return {
+        name: _.head(tracks).album,
+        tracksCount: tracks.length,
+        duration: _.reduce(tracks,(sum,n) => sum + parseInt(n.durationMillis),0),
+        tracks: tracks.map(track => {
+          return {
+            name: track.title,
+            duration: parseInt(track.durationMillis)
+          }
+        })
+      };
+    });
+    return {
+      name: _.head(artistTracks).artist,
+      albumsCount: albums.length,
+      tracksCount: _.reduce(albums,(sum,n) => sum + n.tracksCount,0),
+      duration: _.reduce(albums,(sum,n) => sum + n.duration,0),
+      albums: albums
+    };
+  },
+  /**
+   * Builds the library from the raw GPM JSON and turns into generic artist data
+   * @param library Raw GPM JSON
+   * @returns Generalize list of artist data
+   */
+  buildLibrary(library) {
+    var artistGrouped = _.toArray(_.groupBy(library,'artist'));
+    var artist = artistGrouped.map(this.formatArtist);
+    return {
+      'artistCount': artist.length,
+      'artists': artist
+    };
+  },
+  loadTracks(pm,accu,npt,cb) {
+    var opts = {};
+    if(npt) {
+      opts.nextPageToken = npt;
+    }
+    pm.getAllTracks(opts,(err,library) => {
+      if(library && library.data && library.data.items) {
+        var t = _.concat(accu,library.data.items);
+        if(library.nextPageToken) {
+          this.loadTracks(pm,t,library.nextPageToken,cb);
+        } else {
+          cb(this.buildLibrary(_.flatten(t)));
+        }
+      }
+    })
+  },
+  load(res) {
+    if(cache) {
+      console.log('cache hit');
+      res.send(cache);
+    } else {
+      var pm = new PlayMusic();
+      pm.init({email: "", password: ""}, err => {
+        if (err) console.error(err);
+        this.loadTracks(pm, [], null, d => {
+          cache = d;
+          res.send(d)
+        });
+      });
+    }
+  }
+};
+
+app.options('*',cors());
+
+app.get('/', cors(),function (req, res) {
+  GooglePlayService.load(res);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
---
+++ examples.js
@@ -0,0 +1,44 @@
+//Track definition
+var track = { "kind": "sj#track",
+  "id": "2b0782d2-57f5-3b41-8751-c4c5b0c5475f",
+  "clientId": "f608a557-ce38-45e2-b776-cce349a72363",
+  "creationTimestamp": "1402951576773062",
+  "lastModifiedTimestamp": "1402951576773062",
+  "recentTimestamp": "1402951576771000",
+  "deleted": false,
+  "title": "Lunar Wilderness",
+  "artist": "Between The Buried and Me",
+  "composer": "",
+  "album": "The Parallex: Hypersleep Dialogues",
+  "albumArtist": "Between The Buried and Me",
+  "year": 2011,
+  "trackNumber": 3,
+  "genre": "Pop",
+  "durationMillis": "502000",
+  "albumArtRef": [
+  {
+    "url": "http://lh4.ggpht.com/msVH6LmEaUbeL4b_j8_0j5qrhEatci4reebniO0CiQ1SvUhL0nnS6vu_RasgiaP95h0yxsh_"
+  }
+],
+  "artistArtRef": [
+  {
+    "url": "http://lh3.googleusercontent.com/HW0wtzxoMxyYuVlOWMtZHQGdnsVjqCfRagE5sldP5eJOZEEOS8wQ1JIPapWvbv14QnEaXG_ffgQ"
+  }
+],
+  "discNumber": 1,
+  "estimatedSize": "20084016",
+  "trackType": "8",
+  "storeId": "Tk5qj4i2w56kekbvafy4qzupcdq",
+  "albumId": "Brppuc2axirhjeqk76tqvfjf67y",
+  "artistId": [
+  "Asxzqaupsjmn7c6klzmmzdcto6u"
+],
+  "nid": "Tk5qj4i2w56kekbvafy4qzupcdq",
+  "contentType": "2"
+}
+
+//{
+//  "kind": "sj#trackList",
+//  "nextPageToken": "KnIKSvfiT9Uv/////zzrd/Wt9qVz/wD+//6Ynpaexc/Pz8/Pz5qdyJnPx5mZzsbFy8zFy8zei43FycbMzc3KyMnPzsfGzc7Nxs7Myv/+EOgHIdsN2x+W17J8OQAAAADQKrAdSAFQAFoLCTHyBKjmfYa0EAEyDQoLCgAoo62JpKnAyAI=",
+//  "data": []
+//}
